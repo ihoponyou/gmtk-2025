8b3c90c3ecef586232a0a7faf1a4da58e1c05379
Noah Magcalas <noah.magcalas@gmail.com>
2025-07-31 08:50:41 -0500

    feat: allow drawing

--- foreground.gd
+++ foreground.gd
@@ -1,11 +1,12 @@
 extends TileMapLayer
 
-const TILE_SET_SOURCE_ID = 1
-const BACKGROUND_TILE_POS = Vector2i(1, 1)
-const COOL_TILE_POS = Vector2i(12, 8)
+const TILE_SET_SOURCE_ID := 1
+const BACKGROUND_TILE_POS := Vector2i(1, 1)
+const COOL_TILE_POS := Vector2i(12, 8)
 
 var last_tile_atlas_coords: Vector2i = Vector2i(-1, -1)
 var last_tile_map_pos: Vector2i = Vector2i.ZERO
+var should_replace := false
 
 func _process(delta: float) -> void:
 	var mouse_pos := get_local_mouse_position()
@@ -13,9 +14,10 @@ func _process(delta: float) -> void:
 	if tile_pos == last_tile_map_pos:
 		return
 	
-	set_cell(last_tile_map_pos, TILE_SET_SOURCE_ID, last_tile_atlas_coords)
-	last_tile_atlas_coords = get_cell_atlas_coords(tile_pos)
+	should_replace = not Input.is_action_pressed("primary")
+	if should_replace:
+		set_cell(last_tile_map_pos, TILE_SET_SOURCE_ID, last_tile_atlas_coords)
+		last_tile_atlas_coords = get_cell_atlas_coords(tile_pos)
 	
 	last_tile_map_pos = tile_pos
 	set_cell(tile_pos, TILE_SET_SOURCE_ID, COOL_TILE_POS)
-	print(tile_pos)
